2e9f8447b53608a155ce39ff7c189d2523962018
Miro Salvagni <arpa13572104@ms10lxarpa.regione.fvg.it>
2016-07-20 16:59:25 +0200

    removed dependencies from main.html, fixed pratiche.js

--- routes/pratiche.js
+++ routes/pratiche.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
     sql(function(err,connection) {
 		var query = "select Pratiche.id, Pratiche.idGestore, Pratiche.idComune, Pratiche.address, Pratiche.sitecode, Pratiche.tipopratica, Pratiche.protoIN, Pratiche.dataIN, Pratiche.protoOUT, Pratiche.dataOUT, Pratiche.note, Gestori.name as nameGestore, Comuni.name as nameComune, ConstTipoPratiche.descrizione as nameTipo, B.idUtente, B.idStato, B.timePoint, B.stringStato FROM Pratiche INNER JOIN Gestori on (Pratiche.idGestore = Gestori.id) INNER JOIN Comuni on (Pratiche.idComune = Comuni.id) INNER JOIN ConstTipoPratiche on (Pratiche.tipopratica = ConstTipoPratiche.id) LEFT JOIN (select StatoPratiche.idPratica, StatoPratiche.idUtente, StatoPratiche.idStato, StatoPratiche.timePoint, ConstStatoPratiche.descrizione as stringStato FROM StatoPratiche INNER JOIN ConstStatoPratiche on (StatoPratiche.idStato = ConstStatoPratiche.id)) AS B on (Pratiche.id = B.idPratica) ORDER BY Pratiche.ID;";
 		
-		connection.query(query, function(err, rows) {
+		connection.query(query, function(err, data) {
             if (err) rest.error500(err);
 			else res.json(data);
         });
@@ -36,7 +36,7 @@ router.post('/', function(req, res, next) {
     sql(function (err, connection) {
         var query = mysql.format('INSERT INTO ??(idGestore, idComune, address, sitecode, tipopratica, protoIN, dataIN, protoOUT, dataOUT, note) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [tableName, req.body.idGestore, req.body.idComune, req.body.address, req.body.sitecode, req.body.tipopratica, req.body.protoIN, req.body.dataIN, req.body.protoOUT, req.body.dataOUT, req.body.note]);
 	
-        connection.query(query, function(err, rows) {
+        connection.query(query, function(err, data) {
             if (err) rest.error500(err);
             else rest.created(res, data);
         });
@@ -48,7 +48,7 @@ router.put('/:id', function(req, res, next) {
     sql(function (err, connection) {
         var query = mysql.format('UPDATE ?? SET idGestore = ?, idComune = ?, address = ?, sitecode = ?, tipopratica = ?, protoIN = ?, dataIN = ?, protoOUT = ?, dataOUT = ?, note = ? WHERE id = ?', [tableName, req.body.idGestore, req.body.idComune, req.body.address, req.body.sitecode, req.body.tipopratica, req.body.protoIN, req.body.dataIN, req.body.protoOUT, req.body.dataOUT, req.body.note, req.params.id]);
 
-        connection.query(query, function(err, rows) {
+        connection.query(query, function(err, data) {
             if (err) rest.error500(err);
             else rest.updated(res, data);
         });
@@ -60,7 +60,7 @@ router.delete('/:id', function(req, res, next) {
     sql(function (err, connection) {
 		var query = mysql.format('DELETE FROM ?? WHERE id=?', [tableName, req.params.id]);
 		
-        connection.query(query, function(err, rows) {
+        connection.query(query, function(err, data) {
             if (err) rest.error500(err);
             else rest.deleted(res, data);
         });
