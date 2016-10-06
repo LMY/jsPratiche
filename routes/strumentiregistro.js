5768e3b7354ec10d7cf33dee2a1825e57d88ae0f
Miro Salvagni <miro.salvagni@gmail.com>
2016-10-06 11:24:21 +0200

    strumentiregistro/latest (untested)

--- routes/strumentiregistro.js
+++ routes/strumentiregistro.js
@@ -6,6 +6,49 @@ var tableName = 'RegistroStrumenti';
 var express = require('express');
 var router = express.Router();
 
+router.get('/latest', function(req, res, next) {
+    sql(function(err,connection) {
+		
+        connection.query('SELECT * FROM Catene INNER JOIN (SELECT * FROM RegistroStrumenti WHERE id IN (SELECT id FROM (SELECT MAX(id) as id, idCatena FROM RegistroStrumenti GROUP BY idCatena))) ON Catene.id = RegistroStrumenti.idCatena', function(err, data) {
+            if (err) rest.error500(res, err);
+			else res.json(data);
+		});
+    });
+});
+
+router.put('/latest/:id', function(req, res, next) {
+	var query = "";
+	
+	if (req.body.verb == 'open')
+		query = mysql.format("INSERT INTO ??(??,??,??,??,??) VALUES (?,?,?,?,?)", [tableName, "idCatena", "idUtente", "timePointFrom", "timePointTo", "idSeteTo", req.params.id, req.body.idUtente, req.body.timePointFrom, req.body.timePointTo, req.body.idSeteTo ]);
+	else if (req.body.verb == 'close')
+//		query = mysql.format('UPDATE ?? SET ?? = ?, ?? = ? WHERE id=?', [tableName, "timePointTo", req.body.timePointTo, "idSedeTo", req.body.idSedeTo, req.params.id]);
+		query = mysql.format('UPDATE ?? SET ?? = ?, ?? = ? WHERE id=(SELECT MAX(id) as id WHERE idCatena = ? FROM RegistroStrumenti)', [tableName, "timePointTo", req.body.timePointTo, "idSedeTo", req.body.idSedeTo, req.params.id]);
+	else {
+		rest.error500(res, 'no verb specified');
+		return;
+	}	
+	
+    sql(function(err,connection) {
+		connection.query(query, function(err, data) {
+			if (err) rest.error500(res, err);
+			else res.json(data);
+		});
+    });
+});
+
+router.get('/latest/:id', function(req, res, next) {
+    sql(function(err,connection) {
+		var query = mysql.format('SELECT * FROM ?? WHERE id IN (SELECT MAX(id) as id WHERE idCatena = ? FROM RegistroStrumenti)', [tableName, req.params.id]);
+		
+        connection.query(query, function(err, data) {
+            if (err) rest.error500(res, err);
+			else res.json(data.length == 1 ? data[0] : []);
+		});
+    });
+});
+
+// standard functions
 router.get('/', function(req, res, next) {
     sql(function(err,connection) {
 		
