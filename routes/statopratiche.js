2bcbf72c8e36ca31093f05c94b53709ef68c8e7a
Miro Salvagni <miro.salvagni@gmail.com>
2016-10-13 10:18:34 +0200

    pratiche.get*, statopratiche.post w/o arrivate integrazioni

--- routes/statopratiche.js
+++ routes/statopratiche.js
@@ -35,7 +35,7 @@ router.get('/tipi', function(req, res, next) {
 
 router.get('/history/:id', function(req, res, next) {
     sql(function(err,connection) {
-		var query = mysql.format('SELECT StatoPratiche.*, ConstStatoPratiche.descrizione as descStato, A.username as usernameAss, B.username as usernameMod FROM StatoPratiche LEFT JOIN ConstStatoPratiche on StatoPratiche.idStato=ConstStatoPratiche.id LEFT JOIN AssStatoPraticheUtenti on StatoPratiche.id = AssStatoPraticheUtenti.idStato LEFT JOIN Utenti AS A on idUtente=A.id LEFT JOIN Utenti AS B on idUtenteModifica=B.id WHERE idPratica=?', [req.params.id]);
+		var query = mysql.format('SELECT StatoPratiche.*, ConstStatoPratiche.descrizione as descStato, A.username as usernameAss, B.username as usernameMod, Integrazioni.id as integID, Integrazioni.dateOUT, Integrazioni.dateIN, Integrazioni.protoOUT, Integrazioni.protoIN, Integrazioni.note as noteInteg FROM StatoPratiche LEFT JOIN ConstStatoPratiche on StatoPratiche.idStato=ConstStatoPratiche.id LEFT JOIN AssStatoPraticheUtenti on StatoPratiche.id = AssStatoPraticheUtenti.idStato LEFT JOIN Utenti AS A on idUtente=A.id LEFT JOIN Utenti AS B on idUtenteModifica=B.id LEFT JOIN AssStatoPraticheIntegrazioni ON StatoPratiche.id = AssStatoPraticheIntegrazioni.idStato LEFT JOIN Integrazioni ON AssStatoPraticheIntegrazioni.idInteg = Integrazioni.id WHERE idPratica=?', [req.params.id]);
 
         connection.query(query, function(err, data) {
             if (err) rest.error500(res, err);
@@ -63,38 +63,81 @@ router.post('/', function(req, res, next) {
 		connection.query('START TRANSACTION;', function(err, data) {
 			if (err) rest.error500(res, err);
 			else {
-				if (!req.body.dataOUT) {
-					rest.error500(res, "dataOUT not specified");
-					return;
-				}
+				var query =  mysql.format("INSERT INTO StatoPratiche(idPratica,idStato,idUtenteModifica) VALUES (?,?,?)", [ req.body.idPratica, req.body.idStato, userid ]);
 
-				var query =  mysql.format("INSERT INTO ??(idPratica,idUtente,idStato,idUtenteModifica) VALUES (?,?,?,?)", [tableNameHistory, req.body.idPratica, req.body.idUtente, req.body.idStato, userid ]);
-console.log(query);
 				connection.query(query, function(err, data) {
 					if (err) rest.error500(res, err);
 					else {
 						if (req.body.idStato == 7) {	// richiedi integrazioni
-							var query3 = mysql.format("INSERT INTO Integrazioni(idPratica, dateOUT, dateIN, protoOUT, protoIN, note) VALUES (?,?,NULL,?,NULL,?)", [ req.body.idPratica, req.body.integData, req.body.integProto, req.body.integNote]);
-console.log(query3);
-							connection.query(query3, function(err, data) {
+							if (!req.body.integData) {
+								rest.error500(res, "integData not specified");
+								return;
+							}
+
+							connection.query("SELECT LAST_INSERT_ID() AS id", function(err, laststatoid) {
 								if (err) rest.error500(res, err);
 								else {
-									connection.query('COMMIT;', function(err, data) {
+									var query3 = mysql.format("INSERT INTO Integrazioni(dateOUT, dateIN, protoOUT, protoIN, note) VALUES (?,NULL,?,NULL,?)", [ req.body.integData, req.body.integProto, req.body.integNote]);
+
+									connection.query(query3, function(err, data) {
 										if (err) rest.error500(res, err);
-										else rest.created(res, data);
+										else
+											connection.query("SELECT LAST_INSERT_ID() AS id", function(err, lastintegid) {
+												if (err) rest.error500(res, err);
+												else {
+													var query4 = mysql.format("INSERT INTO AssStatoPraticheIntegrazioni(idStato,idInteg) VALUES (?,?)", [ laststatoid[0].id, lastintegid[0].id ]);
+
+													connection.query(query4, function(err, data) {
+														if (err) rest.error500(res, err);
+														else
+															connection.query('COMMIT;', function(err, data) {
+																if (err) rest.error500(res, err);
+																else rest.created(res, data);
+															});
+													});
+												}
+										});
+									});//Integrazioni(idPratica, dateOUT, dateIN, protoOUT, protoIN, note)
+								}
+							});
+						}
+						// TODO
+						else if (req.body.idStato == 2 && req.body.integData) {	// lavorazione (arrivate integrazioni)
+							if (!req.body.dataOUT) {
+								rest.error500(res, "dataOUT not specified");
+								return;
+							}
+							connection.query("SELECT LAST_INSERT_ID() AS id", function(err, laststatoid) {
+								if (err) rest.error500(res, err);
+								else {
+									var query3 = mysql.format("UPDATE Integrazioni SET dateIN=?,protoIN=? WHERE idPratica=? AND dateOUT=?", [ req.body.integData, req.body.integProto, req.body.idPratica, req.body.dataOUT]);
+
+									connection.query(query3, function(err, data) {
+										if (err) rest.error500(res, err);
+										else {
+											connection.query('COMMIT;', function(err, data) {
+												if (err) rest.error500(res, err);
+												else rest.created(res, data);
+											});
+										}
 									});
 								}
-							});//Integrazioni(idPratica, dateOUT, dateIN, protoOUT, protoIN, note)
+							});
 						}
-						else if (req.body.idStato == 2 && req.body.integData) {	// lavorazione (arrivate integrazioni)
-							var query3 = mysql.format("UPDATE Integrazioni SET dateIN=?,protoIN=? WHERE idPratica=? AND dateOUT=?", [ req.body.integData, req.body.integProto, req.body.idPratica, req.body.dataOUT]);
-
-							connection.query(query3, function(err, data) {
+						else if (req.body.idStato == 2) {	// lavorazione
+							connection.query("SELECT LAST_INSERT_ID() AS id", function(err, lastid) {
 								if (err) rest.error500(res, err);
 								else {
-									connection.query('COMMIT;', function(err, data) {
+									var query3 = mysql.format("INSERT INTO AssStatoPraticheUtenti(idStato,idUtente) VALUES (?,?)", [ lastid[0].id, req.body.idUtente]);
+
+									connection.query(query3, function(err, data) {
 										if (err) rest.error500(res, err);
-										else rest.created(res, data);
+										else {
+											connection.query('COMMIT;', function(err, data) {
+												if (err) rest.error500(res, err);
+												else rest.created(res, data);
+											});
+										}
 									});
 								}
 							});
