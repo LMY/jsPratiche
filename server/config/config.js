7fada5a501b74aea0f985219d752f396fd228bf2
Miro Salvagni <miro.salvagni@gmail.com>
2017-01-18 15:10:35 +0100

    mssql

--- server/config/config.js
+++ server/config/config.js
@@ -17,5 +17,16 @@ module.exports = {
         database : 'jsPratiche',
 		dateStrings: 'date',
         debug    :  false
-	}	
+	},
+	'mssql': {
+		user: '',
+		password: '',
+		server: '',
+		database: '',
+		port: 1433,
+		options: {
+			encrypt: false,
+			tdsVersion: '7_1'	// https://github.com/patriksimek/node-mssql/issues/36 issue with mssql2000
+		}
+	}
 };
---
+++ server/helpers/mssql.js
@@ -0,0 +1,40 @@
+const mssql = require('mssql');
+const config = require('../config/config.js');
+
+module.exports = {
+
+	connect: function(callback, errcallback) {
+		const conn = new mssql.Connection(config.mssql);
+		conn.connect(function(err) {
+			if (err) {
+				conn.close();
+				errcallback(err);
+			}
+			else {
+				callback(conn);
+				conn.close();
+			}
+		});
+	},
+
+	query: function(queryString, callback, errcallback) {
+		const conn = new mssql.Connection(config.mssql);
+		conn.connect(function(err) {
+			if (err) {
+				conn.close();
+				errcallback(err);
+			}
+			else {
+				const req = new mssql.Request(conn);
+				req.query(queryString).then(function (recordset) {
+					conn.close();
+					callback(recordset);
+				})
+				.catch(function (err) {
+					conn.close();
+					errcallback(err);
+				});
+			}
+		});
+	}
+}
