81bb79da55d198005008ad77b085840a64b9769a
Miro Salvagni <miro.salvagni@gmail.com>
2017-01-19 12:05:53 +0100

    get sites by proximity, with cells

--- server/routes/srb/srb.js
+++ server/routes/srb/srb.js
@@ -4,7 +4,7 @@ var mssql = require('../../helpers/mssql.js');
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/all', function(req, res, next) {
 	var query = "SELECT distinct dbo.TBL_SITI.id_sito as ID, dbo.TBL_SITI.codice_sito as 'Codice Sito', dbo.TBL_SITI.stato_parere as 'Stato Parere', "+
 				"dbo.TBL_GESTORI.descrizione as Gestore, dbo.TBL_SITI.comune as Comune, "+
 				"dbo.TBL_SITI.provincia as Provincia, dbo.TBL_SITI.indirizzo as Indirizzo, "+
@@ -20,32 +20,7 @@ router.get('/', function(req, res, next) {
 		if (err) errorHandler(err);
 		else
 			new mssql.mssql.Request(connection)
-				.batch(query, function(err, data2) {
-					if (err) errorHandler(err);
-					else {
-						connection.close();
-						res.json(data2);
-					}
-				});	
-	});
-});
-
-router.get('/cells/:id', function(req, res, next) {
-
-	var query_cells = "SELECT dbo.TBL_CELLE.Sito as IDSito, dbo.TBL_CELLE.Marca_Antenna, dbo.TBL_CELLE.Modello_Antenna, dbo.TBL_CELLE.Altezza, " +
-				"dbo.TBL_SITI.Quota_slm as Quota, dbo.TBL_CELLE.Direzione, dbo.TBL_CELLE.Potenza, dbo.TBL_CELLE.Guadagno as Gain, " +
-				"dbo.TBL_CELLE.Tilt_Mecc as TiltM, dbo.TBL_CELLE.Tilt_Elettrico as TiltE, dbo.TBL_CELLE.UTMX as X, dbo.TBL_CELLE.UTMY as Y, dbo.TBL_CELLE.ALFA24, Flag_sistema, dbo.TBL_CELLE.FREQUENZA " +
-				"FROM dbo.TBL_CELLE INNER JOIN dbo.TBL_SITI on (dbo.TBL_CELLE.Sito = dbo.TBL_SITI.ID_SITO) " +
-				"WHERE Sito = @reqparamsid";
-
-	const errorHandler = function(err) { rest.error500(res, err); connection.close(); }
-
-	mssql.connect(function(err, connection) {
-		if (err) errorHandler(err);
-		else
-			new mssql.mssql.Request(connection)
-				.input('reqparamsid', mssql.mssql.NVarChar, req.params.id)
-				.query(query_cells, function(err, data2) {
+				.batch(query, function(err, data2) {	// batch, not query (res is ~3mb)
 					if (err) errorHandler(err);
 					else {
 						connection.close();
@@ -55,7 +30,21 @@ router.get('/cells/:id', function(req, res, next) {
 	});
 });
 
-router.get('/:id', function(req, res, next) {
+function getCellsOfSite(connection, id, callback) {
+	var query_cells = "SELECT dbo.TBL_CELLE.Sito as IDSito, dbo.TBL_CELLE.Marca_Antenna, dbo.TBL_CELLE.Modello_Antenna, dbo.TBL_CELLE.Altezza, " +
+				"dbo.TBL_SITI.Quota_slm as Quota, dbo.TBL_CELLE.Direzione, dbo.TBL_CELLE.Potenza, dbo.TBL_CELLE.Guadagno as Gain, " +
+				"dbo.TBL_CELLE.Tilt_Mecc as TiltM, dbo.TBL_CELLE.Tilt_Elettrico as TiltE, dbo.TBL_CELLE.UTMX as X, dbo.TBL_CELLE.UTMY as Y, dbo.TBL_CELLE.ALFA24, Flag_sistema, dbo.TBL_CELLE.FREQUENZA " +
+				"FROM dbo.TBL_CELLE INNER JOIN dbo.TBL_SITI on (dbo.TBL_CELLE.Sito = dbo.TBL_SITI.ID_SITO) " +
+				"WHERE Sito = @reqparamsid";
+
+	new mssql.mssql.Request(connection)
+		.input('reqparamsid', mssql.mssql.NVarChar, id)
+		.query(query_cells, function(err, data2) {
+			callback(err, data2);
+		});
+}
+
+function getSite(connection, id, callback) {
 	var query = "SELECT distinct dbo.TBL_SITI.id_sito as ID, dbo.TBL_SITI.codice_sito as 'Codice Sito', dbo.TBL_SITI.stato_parere as 'Stato Parere', "+
 				"dbo.TBL_GESTORI.descrizione as Gestore, dbo.TBL_SITI.comune as Comune, "+
 				"dbo.TBL_SITI.provincia as Provincia, dbo.TBL_SITI.indirizzo as Indirizzo, "+
@@ -66,35 +55,98 @@ router.get('/:id', function(req, res, next) {
 				"WHERE dbo.TBL_CELLE.Sito = dbo.TBL_SITI.id_sito AND dbo.TBL_SITI.ID_SITO = @reqparamsid AND "+
 				"dbo.TBL_SITI.Flag_realizzato = TempRealizzato.id_flag"
 
-	var query_cells = "SELECT dbo.TBL_CELLE.Sito as IDSito, dbo.TBL_CELLE.Marca_Antenna, dbo.TBL_CELLE.Modello_Antenna, dbo.TBL_CELLE.Altezza, " +
-				"dbo.TBL_SITI.Quota_slm as Quota, dbo.TBL_CELLE.Direzione, dbo.TBL_CELLE.Potenza, dbo.TBL_CELLE.Guadagno as Gain, " +
-				"dbo.TBL_CELLE.Tilt_Mecc as TiltM, dbo.TBL_CELLE.Tilt_Elettrico as TiltE, dbo.TBL_CELLE.UTMX as X, dbo.TBL_CELLE.UTMY as Y, dbo.TBL_CELLE.ALFA24, Flag_sistema, dbo.TBL_CELLE.FREQUENZA " +
-				"FROM dbo.TBL_CELLE INNER JOIN dbo.TBL_SITI on (dbo.TBL_CELLE.Sito = dbo.TBL_SITI.ID_SITO) " +
-				"WHERE Sito = @reqparamsid";
+
+	const errorHandler = function(err) { connection.close(); callback(err, []); }
+
+	new mssql.mssql.Request(connection)
+		.input('reqparamsid', mssql.mssql.NVarChar, id)
+		.query(query, function(err, data1) {
+			if (err) errorHandler(err);
+			else
+				getCellsOfSite(connection, id, function(err, data2) {
+
+					if (err) errorHandler(err);
+					else {
+						data1[0].cells = data2;
+						callback(null, data1);
+					}
+				});
+		});
+}
 
 
-	const errorHandler = function(err) { rest.error500(res, err); connection.close(); }
-
+router.get('/near', function(req, res, next) {
+	var query = "SELECT DISTINCT dbo.TBL_SITI.ID_SITO as id " +
+					"FROM dbo.TBL_SITI INNER JOIN dbo.TBL_CELLE ON dbo.TBL_SITI.ID_SITO = dbo.TBL_CELLE.Sito " +
+					"WHERE " +
+					(req.query.favorevoli ? "(dbo.TBL_CELLE.Parere <> 'A' AND dbo.TBL_CELLE.Parere <> 'C') AND ((dbo.TBL_SITI.Flag_realizzato = 1) OR (dbo.TBL_SITI.Flag_realizzato = 4)) AND (dbo.TBL_SITI.Stato_Parere = N'F' OR dbo.TBL_SITI.Stato_Parere = N'L') AND " : "") +
+					"(dbo.TBL_SITI.Tipologia = 0 OR dbo.TBL_SITI.Tipologia = 3) AND " +
+						"((dbo.TBL_SITI.UTMX < @reqparamsx + @reqparamsdist) AND (dbo.TBL_SITI.UTMX > @reqparamsx - @reqparamsdist) AND " +
+						"(dbo.TBL_SITI.UTMY < @reqparamsy + @reqparamsdist) AND (dbo.TBL_SITI.UTMY > @reqparamsy - @reqparamsdist)) "
+					"ORDER BY dbo.TBL_SITI.ID_SITO";
 
 	mssql.connect(function(err, connection) {
+		const errorHandler = function(err) { rest.error500(res, err); connection.close(); }
+
 		if (err) errorHandler(err);
 		else
 			new mssql.mssql.Request(connection)
-				.input('reqparamsid', mssql.mssql.NVarChar, req.params.id)
-				.query(query, function(err, data1) {
+				.input('reqparamsx', mssql.mssql.Real, req.query.x)
+				.input('reqparamsy', mssql.mssql.Real, req.query.y)
+				.input('reqparamsdist', mssql.mssql.Real, req.query.dist)
+				.batch(query, function(err, data2) {
+
 					if (err) errorHandler(err);
-					else
-						new mssql.mssql.Request(connection)
-							.input('reqparamsid', mssql.mssql.NVarChar, req.params.id)
-							.query(query_cells, function(err, data2) {
-								if (err) errorHandler(err);
-								else {
-									connection.close();
-									data1[0].cells = data2;
-									res.json(data1);
-								}
-							});
+					else {
+						const appendi = function(ret, i, data) {		// iterate i=[0;data.length-1]
+							if (i == data.length) {
+								connection.close();
+								res.json(ret);
+							}
+							else
+								getSite(connection, data[i].id, function(err, newdata) {
+									if (err) errorHandler(err);
+									else {
+										ret.push(newdata);
+										appendi(ret, i+1, data);
+									}
+								});
+						};
+
+						appendi([], 0, data2);
+					}
 				});
+	});
+});
+
+
+router.get('/cells/:id', function(req, res, next) {
+
+	mssql.connect(function(err, connection) {
+		if (err) { rest.error500(res, err); connection.close(); }
+		else getCellsOfSite(connection, req.params.id, function(err, data) {
+
+			connection.close();
+
+			if (err) rest.error500(res, err);
+			else res.json(data);
+		});
+	});
+});
+
+router.get('/:id', function(req, res, next) {
+
+	mssql.connect(function(err, connection) {
+		if (err) {
+			connection.close();
+			rest.error500(res, err);
+		}
+		else getSite(connection, req.params.id, function(err, data) {
+			connection.close();
+
+			if (err) rest.error500(res, err);
+			else res.json1(data);
+		});
 	});
 });
 
