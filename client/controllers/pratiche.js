daa2abaadca2bee4716a0413a0d4cc5359079493
Miro Salvagni <miro.salvagni@gmail.com>
2017-02-01 15:42:31 +0100

    merge info pratiche<->siti

--- client/controllers/pratiche.js
+++ client/controllers/pratiche.js
@@ -435,18 +435,40 @@ angular.module('app')
 
 		$scope.requery = function(from, to) {
 			$scope.jspratiche = PraticheInfo.query({ dateFrom: from, dateTo: to }, function() {
+				
+				function getLink(id) {
+					for (var i=0, len=$scope.jspratiche.length; i<len; i++)
+						if ($scope.jspratiche[i].idpratica == id)
+							return $scope.jspratiche[i];
+					
+					return null;
+				}
+				
+				function isCongiunto(linkinfo) {
+					if (!linkinfo) return false;		// no info? can't say.
+					
+					// count #pratiche with that id
+					var count = 0;
+					
+					for (var i=0, len=$scope.jspratiche.length; i<len; i++)
+						if ($scope.jspratiche[i].idpratica == linkinfo.idpratica)
+							++count;
+					
+					return count>1;
+				}
+				
 				$scope.pratiche = PraticheFatturazione.query({ dateFrom: from, dateTo: to }, function() {
 					var seq = 1;
 
 					function extractOperatore(input) {
-						const parts = input.split(' ');
+						const parts = input.split(/[\s-]+/);			// whitespaces and '-'
 
-						for (var i=0; i<parts.length; i++) {
+						for (var i=0, len=parts.length; i<len; i++) {
 							const a = parts[i].trim().toLowerCase();
 							if (a.length == 0 || a == '-')
 								continue;
 
-							for (var u=0; u<$scope.users.length; u++)
+							for (var u=0, lenu=$scope.users.length; u<lenu; u++)
 								if (a == $scope.users[u].username.toLowerCase())
 									return $scope.users[u].username;
 						}
@@ -459,7 +481,7 @@ angular.module('app')
 					function extractRif(input) {
 						const parts = input.split('/');
 
-						for (var i=0; i<parts.length; i++)
+						for (var i=0, len=parts.length; i<len; i++)
 							if (parts[i].includes('-') && !parts[i].includes('P'))
 								return parts[i];
 
@@ -470,7 +492,7 @@ angular.module('app')
 						var mmm = 999999;
 						var already12 = false;
 
-						for (var i=0; i<parts.length; i++) {
+						for (var i=0, len=parts.length; i<len; i++) {
 							var n = parseInt(parts[i]);
 							if (n == NaN) continue;
 
@@ -487,7 +509,7 @@ angular.module('app')
 					}
 					function extractSiteCode(input) {
 						const parts = input.split(' ');
-						for (var i=0; i<parts.length; i++) {
+						for (var i=0, len=parts.length; i<len; i++) {
 							const la = parts[i].trim().toUpperCase();
 
 							if (la.includes("UD") || la.includes("GO")  || la.includes("TS") || la.includes("PN") ||
@@ -499,8 +521,35 @@ angular.module('app')
 					}
 
 					$scope.pratiche.forEach(function(x) {
+					
+						function calcImporto(p1, p2, p3) {
+							if (p1 == '?' || p2 == '?' || p3 == '?') return "?";
+							
+							const q1 = (p1=='1');	// stesso palo?
+							const q2 = (p2=='S');	// singolo?
+							const q3 = (p3=='S');	// sup terra?
+							
+							if (q1 && q2 && q3) return 370;
+							else if (q1 && q2 && !q3) return 300;
+							else if (q1 && !q2 && q3) return 300;
+							else if (q1 && !q2 && !q3) return 150;
+							else if (!q1 && q2 && q3) return 315;
+							else if (!q1 && q2 && !q3) return 300;
+							else if (!q1 && !q2 && q3) return 270;
+							else /*if (!q1 && !q2 && !q3)*/ return 150;							
+						}
+						
+						const linksite = getLink(x.ID_SITO);
+						const congiunto = isCongiunto(linksite);
+						
 						x.seq = seq++;
 						x.FatturareA = '\u2190';
+						
+						x.Punto2Tariffario = linksite ? (linksite.flag87bis ? "1" : "2") : "?";
+						x.SingoloCongiunto = linksite ? (congiunto ? "C" : "S") : "?";
+						x.Punto3Tariffario =  linksite ? (linksite.flagSupTerra ? "S" : "N") : "?";						
+						x.Importo = calcImporto(x.Punto2Tariffario, x.SingoloCongiunto, x.Punto3Tariffario);
+						
 						x.operatore = extractOperatore(x.NoteSito);
 						x.proto = extractProto(x.Pt_RilPar);
 						x.riferimento = extractRif(x.Pt_RilPar);
@@ -511,7 +560,7 @@ angular.module('app')
 			});
 		}
 	}])
-	.controller('PraticheStatistiche', ['$scope', 'Me', 'PMcount','PraticheFatturazione', 'PraticheInfo', 'Utenti', '$location', function($scope, Me, PMcount, PraticheFatturazione, PraticheInfo, Utenti, $location) {
+	.controller('PraticheStatistiche', ['$scope', 'Me', 'PMcount','PraticheFatturazione', 'Utenti', '$location', function($scope, Me, PMcount, PraticheFatturazione, Utenti, $location) {
 		$scope.me = Me.get();
 		$scope.pmcount = PMcount.query();
 		$scope.users = Utenti.query();
@@ -522,92 +571,5 @@ angular.module('app')
 		$scope.orderByField = 'dateIN';
 		$scope.reverseSort = false;
 
-		$scope.show = function(id) {
-			$location.url('pratiche/'+id);
-		}
-
-
-		$scope.requery = function(from, to) {
-			$scope.jspratiche = PraticheInfo.query({ dateFrom: from, to, dateType: "dateOUT" }, function() {
-
-				for (var i=0; i<$scope.jspratiche.length; i++)
-					console.log(JSON.stringify($scope.jspratiche[i]));
-
-				$scope.pratiche = PraticheFatturazione.query({ dateFrom: from, dateTo: to }, function() {
-					var seq = 1;
-
-					function extractOperatore(input) {
-						const parts = input.split(' ');
-
-						for (var i=0; i<parts.length; i++) {
-							const a = parts[i].trim().toLowerCase();
-							if (a.length == 0 || a == '-')
-								continue;
-
-							for (var u=0; u<$scope.users.length; u++)
-								if (a == $scope.users[u].username.toLowerCase())
-									return $scope.users[u].username;
-						}
-
-						return "?!";
-					}
-					function extractData(input) {
-						return moment(input).format('YYYY-MM-DD');
-					}
-					function extractRif(input) {
-						const parts = input.split('/');
-
-						for (var i=0; i<parts.length; i++)
-							if (parts[i].includes('-') && !parts[i].includes('P'))
-								return parts[i];
-
-						return "";
-					}
-					function extractProto(input) {
-						const parts = input.split(' ');
-						var mmm = 999999;
-						var already12 = false;
-
-						for (var i=0; i<parts.length; i++) {
-							var n = parseInt(parts[i]);
-							if (n == NaN) continue;
-
-							if (n == 12 && !already12) {	// ignore first 12
-								already12 = true;
-								continue;
-							}
-
-							if (n < mmm)
-								mmm = n;
-						}
-
-						return mmm == 999999 ? "" : ""+mmm;
-					}
-					function extractSiteCode(input) {
-						const parts = input.split(' ');
-						for (var i=0; i<parts.length; i++) {
-							const la = parts[i].trim().toUpperCase();
-
-							if (la.includes("UD") || la.includes("GO")  || la.includes("TS") || la.includes("PN") ||
-								la.includes("UX") || la.includes("GX")  || la.includes("TX") || la.includes("PX"))
-								return la;
-						}
-
-						return "";
-					}
-
-					$scope.pratiche.forEach(function(x) {
-						x.seq = seq++;
-						x.FatturareA = '\u2190';
-						x.operatore = extractOperatore(x.NoteSito);
-						x.proto = extractProto(x.Pt_RilPar);
-						x.riferimento = extractRif(x.Pt_RilPar);
-						x.data = extractData(x.Dt_RilPar);
-						x.sitecode = extractSiteCode(x.NoteSito);
-					});
-				});
-			});
-		}
-		$scope.requery($scope.dateFrom, $scope.dateTo);	// GET $scope.pratiche
 	}])
 ;
