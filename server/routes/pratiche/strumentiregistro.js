c4abb787d804eb21a9629f280501993212af1131
Miro Salvagni <miro.salvagni@gmail.com>
2017-02-08 10:55:41 +0100

    fix strumenti:close

--- server/routes/pratiche/strumentiregistro.js
+++ server/routes/pratiche/strumentiregistro.js
@@ -30,11 +30,9 @@ router.put('/latest/:id', function(req, res, next) {
 				else {
 					var query2 = sql.format('UPDATE ?? SET timePointFrom = timePointFrom, ?? = ?, ?? = ?  WHERE id=?', [tableName, "timePointTo", req.body.timePointTo, "idSedeTo", req.body.idSedeTo, data[0].id]);
 
-					sql(function(err,connection) {
-						connection.query(query2, function(err, data) {
-							if (err) rest.error500(res, err);
-							else res.json(data);
-						});
+					connection.query(query2, function(err, data) {
+						if (err) rest.error500(res, err);
+						else res.json(data);
 					});
 				}
 			});
@@ -52,41 +50,5 @@ router.get('/', function(req, res, next) {
 		else res.json(data);
 	});
 });
-/*
-router.get('/:id', function(req, res, next) {
-	var query = sql.format('SELECT * FROM ?? WHERE id=?', [tableName, req.params.id]);
-
-	sql.query(query, function(err, data) {
-		if (err) rest.error500(res, err);
-		else res.json(data.length == 1 ? data[0] : []);
-	});
-});
-
-router.delete('/:id', function(req, res, next) {
-	var query = sql.format('DELETE FROM ?? WHERE id=?', [tableName, req.params.id]);
-
-	sql.query(query, function(err, data) {
-		if (err) rest.error500(res, err);
-		else rest.deleted(res, data);
-	});
-});
-
-router.post('/', function(req, res, next) {
-	var query =  sql.format("INSERT INTO ??(??,??,??,??,??) VALUES (?,?,?,?,?)", [tableName, "idCatena", "idUtente", "timePointFrom", "timePointTo", "idSeteTo", req.body.idCatena, req.body.idUtente, req.body.timePointFrom, req.body.timePointTo, req.body.idSeteTo ]);
-
-	connection.query(query, function(err, data) {
-		if (err) rest.error500(res, err);
-		else rest.created(res, data);
-	});
-});
-
-router.put('/:id', function(req, res, next) {
-	var query = sql.format("UPDATE ?? SET ?? = ?, ?? = ?, ?? = ?, ?? = ?, ?? = ? WHERE ?? = ?", [tableName, "idCatena", req.body.idCatena, "idUtente", req.body.idUtente, "timePointFrom", req.body.timePointFrom, "timePointTo", req.body.timePointTo, "idSeteTo", req.body.idSeteTo, "id", req.params.id]);
-
-	connection.query(query, function(err, data) {
-		if (err) rest.error500(res, err);
-		else rest.updated(res, data);
-	});
-});*/
 
 module.exports = router;
