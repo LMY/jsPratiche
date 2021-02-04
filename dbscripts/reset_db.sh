47b2f20f71812641680d4c830547d54c238c6105
Miro Salvagni <miro.salvagni@knorr-bremse.com>
2021-02-05 00:26:05 +0100

    reset_db: execute to clear db
    ./dbscripts/reset_db.sh && npm start
    
    uncomment "sometimes you forget your hases",update and go

---
+++ dbscripts/reset_db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Importing jspratiche" && \
+sudo -u postgres psql --set ON_ERROR_STOP=on dbnir < dbscripts/1.create_pg.psql && \
+echo "Importing DBNIR" && \
+sudo -u postgres psql --set ON_ERROR_STOP=on dbnir < dbscripts/2.create_dbnir.psql
