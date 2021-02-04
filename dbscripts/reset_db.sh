0586694c04bc3fab9da9af5b3e212a8290ddaa90
Miro Salvagni <miro.salvagni@knorr-bremse.com>
2021-02-05 00:42:06 +0100

    create_pg: dbnir init moved to another .psql
    now you can recreate with
    jsPratiche$ ./dbscripts/reset_db.sh && npm start

--- dbscripts/reset_db.sh
+++ dbscripts/reset_db.sh
@@ -2,4 +2,5 @@
 echo "Importing jspratiche" && \
 sudo -u postgres psql --set ON_ERROR_STOP=on dbnir < dbscripts/1.create_pg.psql && \
 echo "Importing DBNIR" && \
-sudo -u postgres psql --set ON_ERROR_STOP=on dbnir < dbscripts/2.create_dbnir.psql
+sudo -u postgres psql --set ON_ERROR_STOP=on dbnir < dbscripts/2.create_dbnir.psql && \
+printf "\n\nOK!\n"
