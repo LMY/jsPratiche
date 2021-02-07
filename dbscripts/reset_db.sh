52c431f8c88f89ab217345172ffc34afa158e8ff
Miro Salvagni <miro.salvagni@knorr-bremse.com>
2021-02-08 00:14:46 +0100

    create_pg: fixture file

--- dbscripts/reset_db.sh
+++ dbscripts/reset_db.sh
@@ -3,4 +3,11 @@ echo "Importing jspratiche" && \
 sudo -u postgres psql --set ON_ERROR_STOP=on dbnir < dbscripts/1.create_pg.psql && \
 echo "Importing DBNIR" && \
 sudo -u postgres psql --set ON_ERROR_STOP=on dbnir < dbscripts/2.create_dbnir.psql && \
+printf "\n\nPopulating db...\n" && \
+sudo -u postgres psql --set ON_ERROR_STOP=on dbnir < dbscripts/4.fixture.psql && \
 printf "\n\nOK!\n"
+
+
+# sudo -u postgres psql dbnir    # -c 
+# UPDATE jspratiche."Utenti" SET hash='' WHERE username='y';
+
