87ce4ca9887b2ad65442c4a989b589f923b76502
miro.salvagni@gmail.com <miro.salvagni@gmail.com>
2021-02-22 23:48:00 +0100

    Catene: /get

--- server/routes/pratiche/catene.js
+++ server/routes/pratiche/catene.js
@@ -5,25 +5,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-
-  var text = 'SELECT ' + sql.tables.Catene +
-  '.*, t2.lab, t2.certn, t2."dateCal", t2.note as "noteCalib", t2.scadenza FROM ' +
-  sql.tables.Catene + ' LEFT JOIN (SELECT * From ' +
-  sql.tables.Calibrazioni +
-  ' WHERE id IN (SELECT id FROM (SELECT id,"idCatena",MAX("dateCal") FROM ' +
-  sql.tables.Calibrazioni + ' GROUP BY "idCatena") AS T1)) AS t2 ON ' +
-  sql.tables.Catene + '.id = t2."idCatena"';
-
-  console.log(text);
-
-
   sql.pool.query(
       'SELECT ' + sql.tables.Catene +
           '.*, t2.lab, t2.certn, t2."dateCal", t2.note as "noteCalib", t2.scadenza FROM ' +
           sql.tables.Catene + ' LEFT JOIN (SELECT * From ' +
           sql.tables.Calibrazioni +
           ' WHERE id IN (SELECT id FROM (SELECT id,"idCatena",MAX("dateCal") FROM ' +
-          sql.tables.Calibrazioni + ' GROUP BY "idCatena") AS T1)) AS t2 ON ' +
+          sql.tables.Calibrazioni + ' GROUP BY "idCatena",id) AS T1)) AS t2 ON ' +
           sql.tables.Catene + '.id = t2."idCatena"',
       function(err, data) {
         if (err)
@@ -40,7 +28,7 @@ router.get('/:id', function(req, res, next) {
           sql.tables.Catene + ' LEFT JOIN (SELECT * FROM ' +
           sql.tables.Calibrazioni +
           ' WHERE id IN (SELECT id FROM (SELECT id,"idCatena",MAX("dateCal") FROM ' +
-          sql.tables.Calibrazioni + ' GROUP BY "idCatena") AS T1)) AS t2 ON ' +
+          sql.tables.Calibrazioni + ' GROUP BY "idCatena",id) AS T1)) AS t2 ON ' +
           sql.tables.Catene + '.id = t2."idCatena" WHERE ' + sql.tables.Catene +
           '.id=$1',
       [req.params.id], function(err, data) {
