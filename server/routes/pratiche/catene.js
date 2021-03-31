c191285661d4c607e0da37257a86922149243338
miro.salvagni@gmail.com <miro.salvagni@gmail.com>
2021-04-01 00:31:11 +0200

    BUGFIX Strumentazione: visualizza l'ultima calibrazione, non la prima!

--- server/routes/pratiche/catene.js
+++ server/routes/pratiche/catene.js
@@ -5,14 +5,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  sql.pool.query(
-      'SELECT ' + sql.tables.Catene +
-          '.*, t2.lab, t2.certn, t2."dateCal", t2.note as "noteCalib", t2.scadenza FROM ' +
-          sql.tables.Catene + ' LEFT JOIN (SELECT * From ' +
-          sql.tables.Calibrazioni +
-          ' WHERE id IN (SELECT id FROM (SELECT id,"idCatena",MAX("dateCal") FROM ' +
-          sql.tables.Calibrazioni + ' GROUP BY "idCatena",id) AS T1)) AS t2 ON ' +
-          sql.tables.Catene + '.id = t2."idCatena"')
+  // https://stackoverflow.com/questions/586781/postgresql-fetch-the-row-which-has-the-max-value-for-a-column
+  sql.pool.query(         
+          'SELECT '+sql.tables.Catene+'.id,name,'+sql.tables.Catene+'.note,l1.lab,l1.certn,l1."dateCal",l1.note as "noteCalib",l1.scadenza '+
+          'FROM '+sql.tables.Catene +' LEFT OUTER JOIN ('+
+          'SELECT l1.* '+
+          'FROM '+sql.tables.Calibrazioni+' l1 LEFT OUTER JOIN '+sql.tables.Calibrazioni+' l2 '+
+          'ON (l1."idCatena" = l2."idCatena" AND (l1.scadenza < l2.scadenza OR (l1.scadenza = l2.scadenza AND l1.id < l2.id))) '+
+          'WHERE l2."idCatena" IS NULL '+
+          'ORDER BY l1."idCatena") l1 '+
+          'ON '+sql.tables.Catene+'.id = l1."idCatena"')
     .then(data => res.json(data.rows))
     .catch(err => rest.error500(res, err));
 });
